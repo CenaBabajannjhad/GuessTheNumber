44337422e4156cdc9693e0ddfe2d7f7ab918ec0d
CenaBabajannjhad <cenababajannjhad@gmail.com>
2024-07-16 21:56:42 +0330

    add some style , delete all logics

--- main.js
+++ main.js
@@ -1,21 +1,12 @@
+// random number generator
 let randomNumber = Math.floor(Math.random()*21);
-console.log(randomNumber)
-let statusToHigh = 'to high';
-let statusToLow = 'to low';
-let statuscorrect = 'correct'; 
-let input = document.getElementById('input')
-let statusContainer = document.getElementById('status');
-let guessButton = document.getElementById('guess');
-
-guessButton.addEventListener('click' , () => {
-    let inputValue = Number(input.value);
-    if(inputValue > randomNumber){
-        statusContainer.textContent = statusToHigh;
-
-    }else if(inputValue < randomNumber){
-        statusContainer.textContent = statusToLow;
-
-    }else if(inputValue === randomNumber){
-        statusContainer.textContent = statuscorrect;
-    }
-})
+// input , statusPlace , button
+const input = document.getElementById('input')
+const statusContainer = document.getElementById('status');
+const startButton = document.getElementById('guess');
+// game status
+const gameStatus = {
+    statusToHigh : 'to high',
+    statusToLow : 'to low' ,
+    statuscorrect : 'correct'
+}
