7197e7be4b92901dd574eec79933577030165e3d
CenaBabajannjhad <cenababajannjhad@gmail.com>
2024-07-18 15:39:24 +0330

    logic finished

--- main.js
+++ main.js
@@ -1,12 +1,72 @@
-// random number generator
-let randomNumber = Math.floor(Math.random()*21);
-// input , statusPlace , button
-const input = document.getElementById('input')
-const statusContainer = document.getElementById('status');
-const startButton = document.getElementById('guess');
-// game status
-const gameStatus = {
-    statusToHigh : 'to high',
-    statusToLow : 'to low' ,
-    statuscorrect : 'correct'
+
+let input = document.getElementById('input')
+let statusContainer = document.getElementById('status');
+let startButton = document.getElementById('start');
+let buttonsContainer = document.getElementById('button-parents');
+let guessButton = document.createElement('button');
+let againButton = document.getElementById('again');
+
+
+// arrow function => get user input value
+let userInputValue = () => Number(input.value);
+// arrow function => reset input
+let resetInputValue = () => input.value = '';
+// arrow function => generate new random number
+let randomNumberGenerator = () => Math.floor(Math.random()*21);
+let randomNumber = randomNumberGenerator();
+
+
+// when start button clicked
+startButton.addEventListener('click' , startTheGame)
+// call this function
+function startTheGame() {
+    // remove input readonly attribute
+    input.removeAttribute('readonly')
+    // append new button and remove start button
+    startButton.remove()
+    guessButton.textContent = 'Guess';
+    guessButton.classList.add('guess-button');
+    guessButton.id = 'guess-button';
+    buttonsContainer.appendChild(guessButton);
+    // change game status text
+    statusContainer.textContent = 'guess the number';
+    // add event listener on guessButton
+    guessButton.addEventListener('click' , checkGuess)
 }
+
+function checkGuess(){
+    // get user value
+    let userValue = userInputValue();
+
+    // check userValue
+    if(userValue){
+        if(userValue > randomNumber){
+            statusContainer.textContent = 'to high ⬆️';
+            resetInputValue();
+
+        }else if(userValue < randomNumber){
+            statusContainer.textContent = 'to low ⬇️';
+            resetInputValue();
+
+        }else if(userValue === randomNumber){
+            document.getElementById('body').classList.add('correct')
+            statusContainer.textContent = 'currect answer 🤞' 
+            againButton.classList.remove('none') 
+        }  
+    }else{
+        alert('you must enter somthing...')
+    }
+}
+
+// addeventlistener on again button
+againButton.addEventListener('click' , resetGame)
+// when clicked reset the game
+function resetGame(){
+    againButton.classList.add('none');
+    resetInputValue(); 
+    randomNumber = randomNumberGenerator();
+    console.log(randomNumber)
+    statusContainer.textContent = 'guess the new number...'
+    document.getElementById('body').classList.remove('correct')
+}
+
